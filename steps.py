daa29515c36af4a0398156602d1be52802c807e8
g-dutra <gdutra42@gmail.com>
2016-08-06 21:33:16 -0300

    Update steps.py
    added challenge 0... oops

--- steps.py
+++ steps.py
@@ -1,3 +1,6 @@
+def challenge0():
+    print(2**38)
+    
 def challenge1():
     #myString = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. "
     myString = "map"
